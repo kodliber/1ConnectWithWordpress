7afacd8c5c6f59cd53ae77d2c641ace00d703611
kodliber <quodliber@gmail.com>
2018-04-16 22:37:43 +0200

    NOTES dans Asyncws
    Signed-off-by: kodliber <quodliber@gmail.com>

--- app/src/main/java/com/example/fredericdinh/a1connectwithwordpress/Asyncws.java
+++ app/src/main/java/com/example/fredericdinh/a1connectwithwordpress/Asyncws.java
@@ -3,17 +3,20 @@ package com.example.fredericdinh.a1connectwithwordpress;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+/**
+ * Asynctask c'est
+ * public abstract class AsyncTask<Params, Progress, Result>
+ */
 public class Asyncws extends AsyncTask<String, Integer, String>
 {
     String url;
 
-    public Asyncws(String url)
+    public Asyncws( String url)
     {
 //        wppost = jso;
         this.url = url;
@@ -32,18 +35,16 @@ public class Asyncws extends AsyncTask<String, Integer, String>
         String retour = "";
         OkHttpClient client = new OkHttpClient();
 
-        //On interroge WordPress via JSON
+        // On interroge WordPress via JSON
 
-        MediaType JSON = MediaType.parse( "application/json; charset=utf-8" );
-        String json = "{'user':'toto','password':'titi'}";
+//        MediaType JSON = MediaType.parse( "application/json; charset=utf-8" );
+//        String json = "{'user':'toto','password':'titi'}";
 
         RequestBody jsonBody = null;
 
-        // dans ce cas, GET ne demande pas de corps
-        // pas besoin d'authentification pour lire le flux de posts
         Request request = new Request.Builder()
-//                .header("Authorization", tokenEncode)
-                .method( "GET", jsonBody )
+//                .header("Authorization", tokenEncode)// pas besoin d'authentification pour lire le flux de posts
+                .method( "GET", jsonBody )// dans ce cas, GET ne demande pas de corps pour la méthode
 //                .url( "https://demo.wp-api.org/wp-json/wp/v2/posts/1" )
                 .url( url )
                 .build();
